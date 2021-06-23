b6e07aca97fb417dfee8f95e087e048319cbd0dc
Carlson <carlson.t.smith@gmail.com>
2021-06-23 14:00:51 -0700

    save tagging change

--- 1D_Dash.py
+++ 1D_Dash.py
@@ -3,7 +3,7 @@ import datetime
 import io
 
 import dash
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, MATCH
 import dash_core_components as dcc
 import dash_html_components as html
 # import dash_table
@@ -41,12 +41,12 @@ app.layout = html.Div([
 ])
 
 
-def parse_contents(contents, filename, date):
+def parse_contents(contents, filename, date, index):
     content_type, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
     try:
-        if 'csv' in filename:
+        if filename.endswith('.csv'):
             # Assume that the user uploaded a CSV file
             df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')))
@@ -64,14 +64,16 @@ def parse_contents(contents, filename, date):
     fig.update_traces(mode='lines+markers')
 
     graphData = [
-            html.H5(filename),
+            html.H5(
+                id={'type': 'filename', 'index': index},
+                children=filename),
             html.H6(datetime.datetime.fromtimestamp(date)),
 
             # Graph of csv file
             dcc.Graph(
                 figure=fig),
             dcc.Dropdown(
-                id='dropdown_tags',
+                id={'type': 'dropdown_tags', 'index': index},
                 options=[
                     {'label': 'Arc', 'value': 'Arc'},
                     {'label': 'Peaks', 'value': 'Peaks'},
@@ -79,8 +81,10 @@ def parse_contents(contents, filename, date):
                     {'label': 'Rods', 'value': 'Rods'}],
                 multi=True,
                 placeholder="Select Tags"),
-            html.Button(id='save-labels', children='Save Labels to Disk'),
-            html.Div(id='saved-response'),
+            html.Button(
+                id={'type': 'save-labels', 'index': index},
+                children='Save Labels to Disk'),
+            html.Div(id={'type': 'saved-response', 'index': index}),
 
             html.Hr(),  # horizontal line
 
@@ -95,24 +99,38 @@ def parse_contents(contents, filename, date):
     return html.Div(graphData)
 
 
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
+@app.callback(
+        Output('output-data-upload', 'children'),
+        Input('upload-data', 'contents'),
+        State('upload-data', 'filename'),
+        State('upload-data', 'last_modified'))
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+        children = []
+        for i in range(len(list_of_contents)):
+            c = list_of_contents[i]
+            n = list_of_names[i]
+            d = list_of_dates[i]
+            children.append(parse_contents(c, n, d, i))
         return children
 
 
-@app.callback(Output('saved-response', 'children'),
-              Input('save-labels', 'n_clicks'),
-              State('dropdown_tags', 'value'))
-def save_labels(n_clicks, list_of_tags):
+@app.callback(
+        Output({'type': 'saved-response', 'index': MATCH}, 'children'),
+        Input({'type': 'save-labels', 'index': MATCH}, 'n_clicks'),
+        State({'type': 'dropdown_tags', 'index': MATCH}, 'value'),
+        State({'type': 'filename', 'index': MATCH}, 'children'),
+        prevent_initial_call=True)
+def save_labels(n_clicks, list_of_tags, file_name):
     if n_clicks is not None and list_of_tags is not None:
-        children = [html.Div('Saved')]
+        # getting rid of .csv from file name to add _tags.csv to end
+        tags_file = file_name[:-4]+'_tags.csv'
+        f = open("./data/tags/"+tags_file, "w")
+        for i in list_of_tags:
+            f.write(i+"\n")
+        f.close()
+
+        children = [html.Div('Saved as {}'.format(tags_file))]
         return children
 
 
